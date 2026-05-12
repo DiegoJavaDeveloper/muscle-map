6ea1893800e3ae3e3db94310cfb8c42e31d9829f
DiegoJavaDeveloper <125951547+DiegoJavaDeveloper@users.noreply.github.com>
2026-05-12 03:18:29 -0300

    Add files via upload

---
+++ paginaweb/style.js
@@ -0,0 +1,7 @@
+function mostrarAlerta() {
+    alert("Esta función se está desarrollando");
+}
+
+function alertaaa(){
+    alert("Esta grupo muscular se encuentra en investigación, pronto se añadirá contenido");    
+}
