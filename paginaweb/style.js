beeac0e01ef2baa571c0c7cba1e1ad6801267e88
DiegoJavaDeveloper <125951547+DiegoJavaDeveloper@users.noreply.github.com>
2026-05-12 08:15:41 -0300

    Delete paginaweb directory

--- paginaweb/style.js
+++
@@ -1,7 +0,0 @@
-function mostrarAlerta() {
-    alert("Esta función se está desarrollando");
-}
-
-function alertaaa(){
-    alert("Esta grupo muscular se encuentra en investigación, pronto se añadirá contenido");    
-}
